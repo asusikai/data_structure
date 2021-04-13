c5d857c97da25ce690ee42f57da09864330174bd
asusikai <75575520+asusikai@users.noreply.github.com>
2021-04-13 13:43:23 +0900

    just

---
+++ tree.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node* treeptr;
+typedef struct node{
+    int data;
+
+    treeptr left;
+    treeptr right;
+}node;
+
+treeptr root = NULL;
+
+void branch_left();
+void branch_right();
+
+
+int main()
+{
+
+}
+
+void branch_left()
+{
+    
+}
+void branch_right();
